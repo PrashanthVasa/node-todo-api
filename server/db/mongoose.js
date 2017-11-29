84c6ce37c3005b37f049c1d8d83ba86897586229
PrashanthVasa <prashanthvasa55@gmail.com>
2017-11-30 01:44:42 +0530

    db changed to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/TodoApp");
-//mongoose.connect("mongodb://adminuser:root@ds125146.mlab.com:25146/todo-api");
+//mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://adminuser:root@ds125146.mlab.com:25146/todo-api");
 
 module.exports ={mongoose};
