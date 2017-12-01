d7f3681e11fbbabf37ce8ac109132cdf4c37d510
PrashanthVasa <prashanthvasa55@gmail.com>
2017-12-01 23:21:28 +0530

    added patch and db and port environments in config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/TodoApp");
-//mongoose.connect("mongodb://adminuser:root@ds125146.mlab.com:25146/todo-api");
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGODB_URI);
 
-module.exports ={mongoose};
+module.exports = {mongoose};
